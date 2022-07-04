932291a34b0c2163f29c72bb5a5be10360585cd6
kaueBAuricchio <46527280+kaueBAuricchio@users.noreply.github.com>
2022-07-04 15:35:13 -0300

    Initial commit

---
+++ app/src/main/java/com/example/navigationcompose/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.navigationcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.navigationcompose.ui.theme.NavigationComposeTheme
+
+class MainActivity : ComponentActivity() {
+
+    lateinit var navController: NavHostController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NavigationComposeTheme {
+
+                navController = rememberNavController()
+                SetupNavGraph(navController = navController)
+
+            }
+        }
+    }
+}
